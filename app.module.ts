d44e15dbbf3df41803a75a2a8a810ac704160c6d
JoshLove-portfolio <60575520+JoshLove-portfolio@users.noreply.github.com>
2021-02-16 22:53:18 -0500

    Commit of updated files
    This update adds mobile-friendly functionality utilizing Angular generate nav component feature.

--- app.module.ts
+++ app.module.ts
@@ -11,6 +11,10 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ResumeComponent } from './resume/resume.component';
 import { AdditionalsComponent } from './additionals/additionals.component';
 import { HomeComponent } from './home/home.component';
+import { NavcomponentComponent } from './navcomponent/navcomponent.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,8 @@ import { HomeComponent } from './home/home.component';
     AboutMeComponent,
     ResumeComponent,
     AdditionalsComponent,
-    HomeComponent
+    HomeComponent,
+    NavcomponentComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +31,10 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatSidenavModule,
-    MatIconModule
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ navcomponent/navcomponent.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navcomponent',
+  templateUrl: './navcomponent.component.html',
+  styleUrls: ['./navcomponent.component.css']
+})
+export class NavcomponentComponent {
+
+	showNav = true;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
